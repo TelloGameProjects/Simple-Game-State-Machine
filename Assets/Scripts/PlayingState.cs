cc9cae538abadf9e199e7cf131718225b4a74d47
Tello- <tellovisionstudio@gmail.com>
2021-08-06 12:27:16 -0700

    Init remote from local

---
+++ Assets/Scripts/PlayingState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+[Serializable]
+public class PlayingState : State
+{
+    private string name = String.Empty;
+
+    #region Properties
+    public string Name
+    {
+        get { return this.name; }
+        protected set { this.name = value; }
+    }
+    #endregion
+
+    #region Class Methods
+    public abstract void OnEnter();
+    public abstract void OnExit();
+    public abstract void Update();
+    public abstract void FixedUpdate();
+    #endregion
+}
