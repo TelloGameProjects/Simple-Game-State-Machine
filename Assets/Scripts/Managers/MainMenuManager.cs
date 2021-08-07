5a8a73d7101238e67288aea817af7c9836e8be5f
Tello- <tellovisionstudio@gmail.com>
2021-08-06 22:01:22 -0700

    Branch for Experimenting

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Vast.StateMachine;
+
+[CreateAssetMenu(fileName = "MainMenuManager", menuName = "Managers/MainMenuManager")]
+public class MainMenuManager : ScriptableObject
+{
+    [SerializeField]
+    private StateMachine appStateMachine;
+
+    public StateMachine AppStateMachine { get => appStateMachine; set => appStateMachine = value; }
+
+    private void Awake()
+    {
+        appStateMachine = new StateMachine();
+    }
+}
