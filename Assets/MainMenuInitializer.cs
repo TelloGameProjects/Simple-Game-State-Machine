4efa5af6199ab227f748ff8de930789213fcc329
Tello- <tellovisionstudio@gmail.com>
2021-08-06 22:26:11 -0700

    Main Menu Initilizer MonoBehavoiur

---
+++ Assets/MainMenuInitializer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vast.StateMachine;
+
+public class MainMenuInitializer : MonoBehaviour
+{
+    [SerializeField] private AppManager managingParent;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        managingParent.AppStateMachine.OnStateChange += CheckChangedState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckChangedState(State newState)
+    {
+
+        // TODO check if newState is relevant to us in MainMenu
+    }
+}
